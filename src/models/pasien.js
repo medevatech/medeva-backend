c9c370c86a1b814d398cbd785066d79c448c8e4c
Helmi Pradita <helmipraditaa@gmail.com>
2023-04-12 13:24:18 +0700

    updating field db feature/registrasi-pasien

--- src/models/pasien.js
+++ src/models/pasien.js
@@ -7,12 +7,14 @@ const insertPasien = (data) => {
     jenis_kelamin,
     nomor_kitas,
     nomor_hp,
-    id_asuransi,
-    alergi,
-    penyakit_kronis,
-    alamat,
     tempat_lahir,
     tanggal_lahir,
+    alamat,
+    kelurahan,
+    kecamatan,
+    kabupaten,
+    provinsi,
+    kode_pos,
     agama,
     kewarganegaraan,
     pekerjaan,
@@ -23,14 +25,14 @@ const insertPasien = (data) => {
     Pool.query(
       `INSERT INTO tbl_pasien 
         (id, nama_lengkap, jenis_kelamin, nomor_kitas, nomor_hp,
-            id_asuransi, alergi, penyakit_kronis, alamat, tempat_lahir,
-            tanggal_lahir, agama, kewarganegaraan, pekerjaan, status_menikah,
-            golongan_darah, created_at, updated_at) 
+            tempat_lahir, tanggal_lahir, alamat, kelurahan, kecamatan,
+            kabupaten, provinsi, kode_pos, agama, kewarganegaraan,
+            pekerjaan, status_menikah, golongan_darah, created_at, updated_at) 
         VALUES
         ('${id}', '${nama_lengkap}', '${jenis_kelamin}', '${nomor_kitas}', '${nomor_hp}',
-            '${id_asuransi}', '${alergi}', '${penyakit_kronis}', '${alamat}', '${tempat_lahir}', 
-            '${tanggal_lahir}', '${agama}', '${kewarganegaraan}', '${pekerjaan}', '${status_menikah}',
-            '${golongan_darah}', NOW(), NOW())`,
+            '${tempat_lahir}', '${tanggal_lahir}', '${alamat}', '${kelurahan}', '${kecamatan}', 
+            '${kabupaten}', '${provinsi}', '${kode_pos}', '${agama}', '${kewarganegaraan}',
+            '${pekerjaan}', '${status_menikah}', '${golongan_darah}', NOW(), NOW())`,
       (err, result) => {
         if (!err) {
           resolve(result);
@@ -45,16 +47,18 @@ const insertPasien = (data) => {
 const allPasien = ({ search, sortBy, sortOrder, limit, offset }) => {
   return new Promise((resolve, reject) =>
     Pool.query(
-      `SELECT tbl_pasien.id, tbl_pasien.nama_lengkap, tbl_pasien.jenis_kelamin, tbl_pasien.nomor_kitas, 
-              tbl_pasien.nomor_hp, tbl_pasien.id_asuransi, tbl_pasien.alergi, tbl_pasien.penyakit_kronis, 
-              tbl_pasien.alamat, tbl_pasien.tempat_lahir, tbl_pasien.tanggal_lahir, tbl_pasien.agama, 
-              tbl_pasien.kewarganegaraan, tbl_pasien.pekerjaan, tbl_pasien.status_menikah, tbl_pasien.golongan_darah,
-              to_char( tbl_pasien.created_at, 'DD Month YYYY - MM:HH' ) AS created_at,
-              to_char( tbl_pasien.updated_at, 'DD Month YYYY - MM:HH' ) AS updated_at
-          FROM tbl_pasien AS tbl_pasien
-          WHERE tbl_pasien.nama_lengkap
-          ILIKE '%${search}%' ORDER BY tbl_pasien.${sortBy} ${sortOrder} 
-          LIMIT ${limit} OFFSET ${offset}`,
+      `SELECT tbl_pasien.id, tbl_pasien.nama_lengkap, tbl_pasien.jenis_kelamin, tbl_pasien.nomor_kitas, tbl_pasien.nomor_hp, 
+        tbl_pasien.tempat_lahir, 
+        to_char( tbl_pasien.tanggal_lahir, 'DD-MM-YYYY' ) AS tanggal_lahir,
+        tbl_pasien.alamat, tbl_pasien.kelurahan, tbl_pasien.kecamatan,
+        tbl_pasien.kabupaten, tbl_pasien.provinsi, tbl_pasien.kode_pos, tbl_pasien.agama, tbl_pasien.kewarganegaraan, 
+        tbl_pasien.pekerjaan, tbl_pasien.status_menikah, tbl_pasien.golongan_darah,
+        to_char( tbl_pasien.created_at, 'DD Month YYYY - HH24:MI' ) AS created_at,
+        to_char( tbl_pasien.updated_at, 'DD Month YYYY - HH24:MI' ) AS updated_at
+      FROM tbl_pasien AS tbl_pasien
+      WHERE tbl_pasien.nama_lengkap
+      ILIKE '%${search}%' ORDER BY tbl_pasien.${sortBy} ${sortOrder} 
+      LIMIT ${limit} OFFSET ${offset}`,
       (err, result) => {
         if (!err) {
           resolve(result);
@@ -73,12 +77,14 @@ const countAllPasien = () => {
 const getPasienById = ({ id }) => {
   return new Promise((resolve, reject) =>
     Pool.query(
-      `SELECT tbl_pasien.id, tbl_pasien.nama_lengkap, tbl_pasien.jenis_kelamin, tbl_pasien.nomor_kitas, 
-        tbl_pasien.nomor_hp, tbl_pasien.id_asuransi, tbl_pasien.alergi, tbl_pasien.penyakit_kronis, 
-        tbl_pasien.alamat, tbl_pasien.tempat_lahir, tbl_pasien.tanggal_lahir, tbl_pasien.agama, 
-        tbl_pasien.kewarganegaraan, tbl_pasien.pekerjaan, tbl_pasien.status_menikah, tbl_pasien.golongan_darah,
-        to_char( tbl_pasien.created_at, 'DD Month YYYY - MM:HH' ) AS created_at,
-        to_char( tbl_pasien.updated_at, 'DD Month YYYY - MM:HH' ) AS updated_at
+      `SELECT tbl_pasien.id, tbl_pasien.nama_lengkap, tbl_pasien.jenis_kelamin, tbl_pasien.nomor_kitas, tbl_pasien.nomor_hp, 
+        tbl_pasien.tempat_lahir, 
+        to_char( tbl_pasien.tanggal_lahir, 'DD-MM-YYYY' ) AS tanggal_lahir,
+        tbl_pasien.alamat, tbl_pasien.kelurahan, tbl_pasien.kecamatan,
+        tbl_pasien.kabupaten, tbl_pasien.provinsi, tbl_pasien.kode_pos, tbl_pasien.agama, tbl_pasien.kewarganegaraan, 
+        tbl_pasien.pekerjaan, tbl_pasien.status_menikah, tbl_pasien.golongan_darah,
+        to_char( tbl_pasien.created_at, 'DD Month YYYY - HH24:MI' ) AS created_at,
+        to_char( tbl_pasien.updated_at, 'DD Month YYYY - HH24:MI' ) AS updated_at
       FROM tbl_pasien AS tbl_pasien
       WHERE tbl_pasien.id = '${id}'`,
       (err, result) => {
@@ -115,12 +121,14 @@ const editPasien = (data) => {
     jenis_kelamin,
     nomor_kitas,
     nomor_hp,
-    id_asuransi,
-    alergi,
-    penyakit_kronis,
-    alamat,
     tempat_lahir,
     tanggal_lahir,
+    alamat,
+    kelurahan,
+    kecamatan,
+    kabupaten,
+    provinsi,
+    kode_pos,
     agama,
     kewarganegaraan,
     pekerjaan,
@@ -132,8 +140,8 @@ const editPasien = (data) => {
       `UPDATE tbl_pasien 
           SET
             nama_lengkap='${nama_lengkap}', jenis_kelamin='${jenis_kelamin}', nomor_kitas='${nomor_kitas}', nomor_hp='${nomor_hp}', 
-            id_asuransi='${id_asuransi}', alergi='${alergi}', penyakit_kronis='${penyakit_kronis}', alamat='${alamat}', 
-            tempat_lahir='${tempat_lahir}', tanggal_lahir='${tanggal_lahir}', agama='${agama}', kewarganegaraan='${kewarganegaraan}', 
+            tempat_lahir='${tempat_lahir}', tanggal_lahir='${tanggal_lahir}', alamat='${alamat}', kelurahan='${kelurahan}', kecamatan='${kecamatan}', 
+            kabupaten='${kabupaten}', provinsi='${provinsi}', kode_pos='${kode_pos}', agama='${agama}', kewarganegaraan='${kewarganegaraan}', 
             pekerjaan='${pekerjaan}', status_menikah='${status_menikah}', golongan_darah='${golongan_darah}', 
             updated_at=NOW()
           WHERE id='${id}'`,
