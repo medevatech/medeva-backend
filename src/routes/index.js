6034446125337cf7c00872ef47249f86e3717e7c
Helmi Pradita <helmipraditaa@gmail.com>
2023-04-12 15:57:02 +0700

    pending diagnosis

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const alergiPasienControllers = require('./alergiPasien');
 const vitalSignsControllers = require('./vitalSigns');
 const kunjunganControllers = require('./kunjungan');
 const penyakitControllers = require('./penyakit');
+const diagnosisControllers = require('./diagnosis');
 
 router.use('/pasien', pasienControllers);
 router.use('/skrining', skriningControllers);
@@ -16,5 +17,6 @@ router.use('/alergi-pasien', alergiPasienControllers);
 router.use('/vital-signs', vitalSignsControllers);
 router.use('/kunjungan', kunjunganControllers);
 router.use('/penyakit', penyakitControllers);
+router.use('/diagnosis', diagnosisControllers);
 
 module.exports = router;
