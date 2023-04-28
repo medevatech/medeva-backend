fe193d9ec7ee06d3eb007bcb7576737e451a1d48
Helmi Pradita <helmipraditaa@gmail.com>
2023-04-28 14:56:22 +0700

    update getAll /pasien with query is_active

--- src/models/pasien.js
+++ src/models/pasien.js
@@ -127,10 +127,9 @@ const getPasienById = ({ id }) => {
         tbl_pasien.alamat, tbl_pasien.kelurahan, tbl_pasien.kecamatan,
         tbl_pasien.kota, tbl_pasien.provinsi, tbl_pasien.kode_pos, tbl_pasien.agama, tbl_pasien.kewarganegaraan, 
         tbl_pasien.pekerjaan, tbl_pasien.status_menikah, tbl_pasien.golongan_darah,
-        tbl_pasien.is_active, tbl_pasien.is_archive,
+        tbl_pasien.is_active, 
           to_char( tbl_pasien.created_at, 'DD Month YYYY - HH24:MI' ) AS created_at,
-          to_char( tbl_pasien.updated_at, 'DD Month YYYY - HH24:MI' ) AS updated_at,
-        tbl_pasien.is_active, tbl_pasien.is_archive
+          to_char( tbl_pasien.updated_at, 'DD Month YYYY - HH24:MI' ) AS updated_at
       FROM tbl_pasien AS tbl_pasien
       WHERE tbl_pasien.id = '${id}'`,
       (err, result) => {
