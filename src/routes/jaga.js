f765c4aa20011411f58948d03e4a80053cefe742
Fanani23 <pramudiasf23@gmail.com>
2023-05-04 11:25:48 +0700

    adding router

--- src/routes/jaga.js
+++ src/routes/jaga.js
@@ -7,6 +7,8 @@ router.post("/", jagaController.create);
 router.get("/", jagaController.get);
 router.get("/:id", jagaController.getById);
 router.put("/:id", jagaController.update);
+router.put("/archive/:id", jagaController.archive);
+router.put("/activate/:id", jagaController.activate);
 router.delete("/:id", jagaController.delete);
 
 module.exports = router;
--- src/routes/shift.js
+++ src/routes/shift.js
@@ -7,6 +7,8 @@ router.post("/", shiftController.create);
 router.get("/", shiftController.get);
 router.get("/:id", shiftController.getById);
 router.put("/:id", shiftController.update);
+router.put("/archive/:id", shiftController.archive);
+router.put("/activate/:id", shiftController.activate);
 router.delete("/:id", shiftController.delete);
 
 module.exports = router;
