0d3a68f8898a8b8b6f97f0ba695c12c04c8d1da1
Fanani23 <pramudiasf23@gmail.com>
2023-05-15 11:47:54 +0700

    add search name on jaga

--- src/models/jaga.js
+++ src/models/jaga.js
@@ -22,6 +22,7 @@ const countJaga = () => {
 
 const getJaga = ({
   searchName,
+  searchNameD,
   searchStatus,
   searchDivisi,
   sortBy,
@@ -32,7 +33,7 @@ const getJaga = ({
   return new Promise((resolve, reject) => {
     pool.query(
       `SELECT tbl_jaga.id, tbl_jaga.id_klinik, tbl_jaga.id_divisi, tbl_jaga.id_shift, tbl_jaga.id_karyawan, tbl_jaga.is_active, tbl_klinik.nama_klinik, tbl_divisi.tipe, tbl_shift.hari, tbl_karyawan.nama as nama_karyawan FROM tbl_jaga as tbl_jaga INNER JOIN tbl_klinik AS tbl_klinik ON tbl_jaga.id_klinik = tbl_klinik.id INNER JOIN tbl_divisi AS tbl_divisi ON tbl_jaga.id_divisi = tbl_divisi.id INNER JOIN tbl_shift AS tbl_shift ON tbl_jaga.id_shift = tbl_shift.id INNER JOIN tbl_karyawan AS tbl_karyawan ON tbl_jaga.id_karyawan = tbl_karyawan.id
-          WHERE tbl_karyawan.nama ILIKE '%${searchName}%' AND tbl_jaga.id_divisi ILIKE '%${searchDivisi}%' AND tbl_jaga.is_active ILIKE '%${searchStatus}%' ORDER BY tbl_jaga.${sortBy} ${sortOrder} LIMIT ${limit} OFFSET ${offset}
+          WHERE tbl_karyawan.nama ILIKE '%${searchName}%' AND tbl_jaga.id_divisi ILIKE '%${searchDivisi}%' AND tbl_divisi.tipe ILIKE '%${searchNameD}%' tbl_jaga.is_active ILIKE '%${searchStatus}%' ORDER BY tbl_jaga.${sortBy} ${sortOrder} LIMIT ${limit} OFFSET ${offset}
         `,
       (err, res) => {
         if (!err) {
