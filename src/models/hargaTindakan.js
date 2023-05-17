3988ac52bf9ca066fd5932731a14c57abb936024
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-17 16:52:46 +0700

    update endpoint harga-tindakan, fixing is_active colum in edit

--- src/models/hargaTindakan.js
+++ src/models/hargaTindakan.js
@@ -104,12 +104,12 @@ const findHargaTindakanById = (id) => {
 };
 
 const editHargaTindakan = (data) => {
-  const { id, id_klinik, id_daftar_tindakan, harga, is_active } = data;
+  const { id, id_klinik, id_daftar_tindakan, harga } = data;
   return new Promise((resolve, reject) =>
     Pool.query(
       `UPDATE tbl_harga_tindakan 
           SET
-            id_klinik='${id_klinik}', id_daftar_tindakan='${id_daftar_tindakan}', harga='${harga}', is_active='${is_active}', 
+            id_klinik='${id_klinik}', id_daftar_tindakan='${id_daftar_tindakan}', harga=${harga}, 
             updated_at=NOW()
           WHERE id='${id}'`,
       (err, result) => {
