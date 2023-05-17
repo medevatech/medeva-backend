30a51fa1449d82a38415a562b618babc66b7291c
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-17 11:11:27 +0700

    update endpoint tindakan, delete column catatan

--- src/controllers/tindakan.js
+++ src/controllers/tindakan.js
@@ -17,7 +17,6 @@ const tindakanControllers = {
       let data = {
         id: uuidv4(),
         id_kunjungan: req.body.id_kunjungan,
-        catatan: req.body.catatan,
       };
 
       await insertTindakan(data);
@@ -103,7 +102,6 @@ const tindakanControllers = {
         let data = {
           id,
           id_kunjungan: req.body.id_kunjungan,
-          catatan: req.body.catatan,
         };
 
         await editTindakan(data);
