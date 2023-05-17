0615b42a6242144ba00edc1808a84dbfa12b728d
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-17 11:02:32 +0700

    update endpoint daftar-tindakan, delete column id_klinik

--- src/models/daftarTindakan.js
+++ src/models/daftarTindakan.js
@@ -32,15 +32,11 @@ const allDaftarTindakan = ({
   return new Promise((resolve, reject) =>
     Pool.query(
       `SELECT tbl_daftar_tindakan.id, tbl_daftar_tindakan.id_tindakan, tbl_daftar_tindakan.nama, 
-          tbl_harga_tindakan.id_klinik,
         tbl_daftar_tindakan.is_active, 
         tbl_daftar_tindakan.created_at, tbl_daftar_tindakan.updated_at
       FROM tbl_daftar_tindakan AS tbl_daftar_tindakan
-      INNER JOIN tbl_harga_tindakan as tbl_harga_tindakan ON tbl_daftar_tindakan.id = tbl_harga_tindakan.id_daftar_tindakan
       WHERE
         tbl_daftar_tindakan.nama ILIKE '%${search}%' 
-      AND
-        tbl_harga_tindakan.id_klinik ILIKE '%${searchKlinik}%'
       ORDER BY tbl_daftar_tindakan.${sortBy} ${sortOrder} 
       LIMIT ${limit} OFFSET ${offset}`,
       (err, result) => {
@@ -62,11 +58,9 @@ const getDaftarTindakanById = ({ id }) => {
   return new Promise((resolve, reject) =>
     Pool.query(
       `SELECT tbl_daftar_tindakan.id, tbl_daftar_tindakan.id_tindakan, tbl_daftar_tindakan.nama, 
-          tbl_harga_tindakan.id_klinik,
         tbl_daftar_tindakan.is_active, 
         tbl_daftar_tindakan.created_at, tbl_daftar_tindakan.updated_at
       FROM tbl_daftar_tindakan AS tbl_daftar_tindakan
-      INNER JOIN tbl_harga_tindakan as tbl_harga_tindakan ON tbl_daftar_tindakan.id = tbl_harga_tindakan.id_daftar_tindakan
       WHERE tbl_daftar_tindakan.id = '${id}'`,
       (err, result) => {
         if (!err) {
