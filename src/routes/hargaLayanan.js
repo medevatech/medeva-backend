b2367c880359cc370beae7a6aaef85d39180e2d2
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-19 15:07:24 +0700

    update endpoint daftar-layanan, take over

--- src/routes/hargaLayanan.js
+++ src/routes/hargaLayanan.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const router = express.Router();
-const { hargaLayananControllers } = require(`../controllers/hargaLayanan`);
+const { hargaLayananControllers } = require(`../controllers/hargaTindakan`);
 const { protect } = require('../middleware/auth');
 let multer = require('multer');
 let uploaded = multer();
@@ -9,5 +9,15 @@ router.post(`/`, uploaded.array(), hargaLayananControllers.add);
 router.get(`/`, hargaLayananControllers.getAll);
 router.get(`/:id`, hargaLayananControllers.getById);
 router.put(`/:id`, uploaded.array(), hargaLayananControllers.edit);
+router.put(
+  `/activate/:id`,
+  uploaded.array(),
+  hargaLayananControllers.editActivate
+);
+router.put(
+  `/archive/:id`,
+  uploaded.array(),
+  hargaLayananControllers.editArchive
+);
 
 module.exports = router;
