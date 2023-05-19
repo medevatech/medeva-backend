6f1b645be0640b36a4c0fd23211c1d99cc60be7a
Fanani23 <pramudiasf23@gmail.com>
2023-05-19 10:11:45 +0700

    remove 1 of 2 jaga create

--- src/routes/jaga.js
+++ src/routes/jaga.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { jagaController } = require("../controllers/jaga");
 
 router.post("/", jagaController.create);
-router.post("/", jagaController.create);
 router.get("/", jagaController.get);
 router.get("/:id", jagaController.getById);
 router.put("/:id", jagaController.update);
