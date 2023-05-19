4d31f8f8e29f2685bb5b1e0c1d65cdd58aa1dae0
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-19 15:15:49 +0700

    update endpoint harga-layanan, take over

--- src/models/hargaLayanan.js
+++ src/models/hargaLayanan.js
@@ -8,7 +8,7 @@ const insertHargaLayanan = (data) => {
         (id, id_klinik, id_daftar_layanan, harga, is_active,
             created_at, updated_at) 
         VALUES
-        ('${id}', '${id_klinik}', '${id_daftar_layanan}', ${harga}, '${is_active}',
+        ('${id}', '${id_klinik}', '${id_daftar_layanan}', ${harga}, ${is_active},
             NOW(), NOW())`,
       (err, result) => {
         if (!err) {
