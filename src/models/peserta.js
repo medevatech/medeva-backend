b57055208885211eb6df68ca136707b91ff42895
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-19 09:57:04 +0700

    update endpoint peserta,  added JOIN pasien and asuransi on getById

--- src/models/peserta.js
+++ src/models/peserta.js
@@ -66,9 +66,12 @@ const getPesertaById = ({ id }) => {
   return new Promise((resolve, reject) =>
     Pool.query(
       `SELECT tbl_peserta.id, 
-          tbl_peserta.id_pasien, tbl_peserta.id_asuransi,
-          tbl_peserta.created_at, tbl_peserta.updated_at
+        tbl_peserta.id_pasien, tbl_pasien.nama_lengkap AS nama_pasien,
+        tbl_peserta.id_asuransi, tbl_asuransi.nama  AS nama_asuransi,
+        tbl_peserta.created_at, tbl_peserta.updated_at
       FROM tbl_peserta AS tbl_peserta
+      INNER JOIN tbl_pasien as tbl_pasien ON tbl_peserta.id_pasien = tbl_pasien.id
+      INNER JOIN tbl_asuransi as tbl_asuransi ON tbl_peserta.id_asuransi = tbl_asuransi.id
       WHERE tbl_peserta.id = '${id}'`,
       (err, result) => {
         if (!err) {
