82b1180bfc311059bbfc2eb8fd03ba16ed5e65f2
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-22 15:08:36 +0700

    update endpoint peserta, fixing

--- src/models/peserta.js
+++ src/models/peserta.js
@@ -127,12 +127,12 @@ const getPesertaByIdPasien = ({ id_pasien }) => {
     Pool.query(
       `SELECT tbl_peserta.id, 
         tbl_peserta.id_pasien, tbl_pasien.nama_lengkap AS nama_pasien,
-        tbl_peserta.id_asuransi, 
+        tbl_peserta.id_asuransi, tbl_asuransi.nama  AS nama_asuransi,
         tbl_peserta.nomor_asuransi, tbl_peserta.is_active,
         tbl_peserta.created_at, tbl_peserta.updated_at
       FROM tbl_peserta AS tbl_peserta
       INNER JOIN tbl_pasien as tbl_pasien ON tbl_peserta.id_pasien = tbl_pasien.id
-      
+      INNER JOIN tbl_asuransi as tbl_asuransi ON tbl_peserta.id_asuransi = tbl_asuransi.id
       WHERE tbl_peserta.id_pasien = '${id_pasien}'`,
       (err, result) => {
         if (!err) {
