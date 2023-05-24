8ebb9a6406222e80bfc9540add285b1fb17104ea
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-24 13:43:51 +0700

    update endpoint rs, fixing ednpoint active, archive and delete

--- src/routes/rs.js
+++ src/routes/rs.js
@@ -9,5 +9,8 @@ router.post(`/`, uploaded.array(), rsControllers.add);
 router.get(`/`, rsControllers.getAll);
 router.get(`/:id`, rsControllers.getById);
 router.put(`/:id`, uploaded.array(), rsControllers.edit);
+router.put(`/activate/:id`, uploaded.array(), rsControllers.editActivate);
+router.put(`/archive/:id`, uploaded.array(), rsControllers.editArchive);
+router.delete(`/:id`, uploaded.array(), rsControllers.delete);
 
 module.exports = router;
