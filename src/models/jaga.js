4579e3c576cf66d25175220a09348810951855f1
Fanani23 <pramudiasf23@gmail.com>
2023-05-29 13:53:18 +0700

    jaga id

--- src/models/jaga.js
+++ src/models/jaga.js
@@ -64,15 +64,15 @@ const getJaga = ({
   });
 };
 
-const getJagaById = ({ id }) => {
+const getJagaById = (id) => {
   return new Promise((resolve, reject) => {
     pool.query(
-      `SELECT SELECT jaga.id, jaga.id_klinik, jaga.id_divisi, jaga.id_karyawan, jaga.hari, jaga.tanggal, jaga.waktu_mulai, jaga.waktu_selesai, jaga.is_active, jaga.created_at, jaga.updated_at, divisi.tipe, kry.nama as nama_karyawan, klinik.nama_klinik
+      `SELECT jaga.id, jaga.id_klinik, jaga.id_divisi, jaga.id_karyawan, jaga.hari, jaga.tanggal, jaga.waktu_mulai, jaga.waktu_selesai, jaga.is_active, jaga.created_at, jaga.updated_at, divisi.tipe, kry.nama as nama_karyawan, klinik.nama_klinik
       FROM tbl_jaga AS jaga
       INNER JOIN tbl_klinik AS klinik ON jaga.id_klinik = klinik.id
       INNER JOIN tbl_divisi AS divisi ON jaga.id_divisi = divisi.id
       INNER JOIN tbl_karyawan AS kry ON jaga.id_karyawan = kry.id
-      WHERE jaga.id_divisi = '${id}'`,
+      WHERE jaga.id = '${id}'`,
       (err, res) => {
         if (!err) {
           resolve(res);
