11f9adf698be1c457d59aae1395ac5d945a50fad
Helmi Pradita <helmipraditaa@gmail.com>
2023-05-29 10:52:35 +0700

    update endpoint skrining, add activate, archive and delete

--- src/routes/skrining.js
+++ src/routes/skrining.js
@@ -9,5 +9,8 @@ router.post(`/`, uploaded.array(), skriningControllers.add);
 router.get(`/`, skriningControllers.getAll);
 router.get(`/:id`, skriningControllers.getById);
 router.put(`/:id`, uploaded.array(), skriningControllers.edit);
+router.put(`/activate/:id`, uploaded.array(), skriningControllers.editActivate);
+router.put(`/archive/:id`, uploaded.array(), skriningControllers.editArchive);
+router.delete(`/:id`, uploaded.array(), skriningControllers.delete);
 
 module.exports = router;
