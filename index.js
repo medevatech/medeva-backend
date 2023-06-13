845c52779287c690ac52a673b277ed1e8b279c3f
Helmi Pradita <helmipraditaa@gmail.com>
2023-06-13 09:14:45 +0700

    update index.js

--- index.js
+++ index.js
@@ -54,12 +54,12 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(morgan('dev'));
