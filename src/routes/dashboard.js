bb669ebc62c76a22483c72d47a19b8a96db5384b
Helmi Pradita <helmipraditaa@gmail.com>
2023-06-28 10:58:42 +0700

    update endpoint dashboard/as-02

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { dashboardController } = require('../controllers/dashboard.js');
 const { protect } = require('../middleware/auth.js');
 
-router.get('/as-02', protect, dashboardController.getAS02);
+router.get('/as-02', dashboardController.getAS02);
 router.get('/as-03', dashboardController.getAS03);
 
 module.exports = router;
