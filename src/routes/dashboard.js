9995fc2d125fd190b9405f4e6eb0dfcf30627ff8
Helmi Pradita <helmipraditaa@gmail.com>
2023-06-29 13:58:44 +0700

    update endpoint dashboard/as-04, update remove hard-code

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -6,7 +6,7 @@ const { protect } = require('../middleware/auth.js');
 router.get('/as-02', protect, dashboardController.getAS02);
 router.get('/as-03', dashboardController.getAS03);
 router.get(
-  '/as-03/analisa-rujukan/567',
+  '/as-03/analisa-rujukan/:id_dokter',
   dashboardController.getAS03AnalisaRujukan
 );
 router.get('/as-04', dashboardController.getAS04);
