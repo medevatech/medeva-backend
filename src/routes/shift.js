4b1a64771535ff457433330152ef981a4b94fdd5
Fanani23 <pramudiasf23@gmail.com>
2023-07-06 11:28:39 +0700

    create shift

--- src/routes/shift.js
+++ src/routes/shift.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { shiftController } = require("../controllers/shift");
-const { protect } = require("../middleware/auth");
+const { nonDoctorController } = require("../controllers/jadwalNonDokter");
+// const { protect } = require("../middleware/auth");
 
-router.post("/", protect, shiftController.create);
-router.post("/", protect, shiftController.create);
-router.get("/", protect, shiftController.get);
-router.get("/:id", protect, shiftController.getById);
-router.get("/clinic/:id", protect, shiftController.getByIdClinic);
-router.put("/:id", protect, shiftController.update);
-router.put("/archive/:id", protect, shiftController.archive);
-router.put("/activate/:id", protect, shiftController.activate);
-router.delete("/:id", protect, shiftController.delete);
+router.post("/", nonDoctorController.create);
+// router.get("/", protect, shiftController.get);
+// router.get("/:id", protect, shiftController.getById);
+// router.get("/clinic/:id", protect, shiftController.getByIdClinic);
+// router.put("/:id", protect, shiftController.update);
+// router.put("/archive/:id", protect, shiftController.archive);
+// router.put("/activate/:id", protect, shiftController.activate);
+// router.delete("/:id", protect, shiftController.delete);
 
 module.exports = router;
